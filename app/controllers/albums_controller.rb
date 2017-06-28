bf671483107cac31b7dc060f5039951f60cf5ee2
jin camou and Susha Dore <jin@>
2017-06-28 13:45:23 -0700

    add album creation

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,24 @@
 class AlbumsController < ApplicationController
 
-  def index
-    @albums = Album.all
+  def new
+    @singer = Singer.find(params[:singer_id])
+    @album = @singer.albums.new
   end
 
-  def show
-    @album = Album.find(params[:id])
+  def create
+    @singer = Singer.find(params[:singer_id])
+    @album = @singer.albums.new(album_params)
+    if @album.save
+      flash[:notice] = "Album successfully added!"
+      redirect_to singer_path(@album.singer)
+    else
+      render :new
+    end
+  end
+
+  private
+  def album_params
+    params.require(:album).permit(:title, :year)
   end
 
 end
